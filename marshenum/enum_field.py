de26979f7156c1143eaeae47c545ec011d1cce4f
Arsen <zidder@hilearn.io>
2020-03-24 13:51:24 +0400

    Check and parse enum type.
    Summary:
        When passing integers as query parameters, it should be parsed to integer before getting enum value
    
    Test Plan:
        none
    
    Reviewers: arsen
    
    Reviewed By: arsen
    
    Differential Revision: https://phabricator.hilearn.io/D3340

--- marshenum/enum_field.py
+++ marshenum/enum_field.py
@@ -5,13 +5,20 @@ from marshmallow.fields import Field
 from marshmallow_annotations import registry
 
 
+BASE_TYPES = [int, float, str, tuple]
+
+
 class EnumField(Field):
     default_error_messages = {
-        'invalid_value': 'Invalid enum value for {cls}: {inpt}'
+        'invalid_value': 'Invalid enum value for {cls}: {inpt}',
+        'invalid_type': 'Enum type is invalid: {inpt} is not {enum_type}'
     }
 
     def __init__(self, cls, *args, **kwargs):
         self._enum_cls = cls
+        self._enum_type = [_type
+                           for _type in BASE_TYPES
+                           if issubclass(cls, _type)][0]
         super().__init__(*args, **kwargs)
 
     def _serialize(self, value, attr, obj):
@@ -20,6 +27,10 @@ class EnumField(Field):
     def _deserialize(self, value, attr, data, **kwargs):
         if value is None:
             return None
+        try:
+            value = self._enum_type(value)
+        except (TypeError, ValueError):
+            self.fail('invalid_type', inpt=value)
         try:
             return self._enum_cls._value2member_map_[value]
         except KeyError:
@@ -34,6 +45,7 @@ class EnumField(Field):
         if key in self.default_error_messages:
             msg = self.default_error_messages[key].format(
                 inpt=kwargs['inpt'],
+                enum_type=self._enum_type.__name__,
                 cls=self._enum_cls.__name__)
             raise ValidationError(msg)
         super().fail(key, **kwargs)
@@ -43,7 +55,7 @@ class RegisteredEnumMeta(EnumMeta):
     def __new__(cls, name, bases, cls_dict):
         if (not any(base_type in base_cls.mro()
                     for base_cls in bases
-                    for base_type in [int, float, str, tuple])
+                    for base_type in BASE_TYPES)
                 and name != 'RegisteredEnum'):
             bases = (str, *bases)
         return super().__new__(cls, name, bases, cls_dict)
