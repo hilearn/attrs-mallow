14e1f358dfd32e49f9317d14cea267d9240ca189
Arsen <zidder@hilearn.io>
2020-06-24 08:36:34 +0400

    Fix marshmallow schema for exclusions
    Summary:
    Usage:
    ```
    @attr_with_schema(register_as_scheme=True, strict=True)
    @attr.s(auto_attribs=True)
    class Base:
        obj: dict
        integer: int = 0
    
    @attr_with_schema(register_as_scheme=True, strict=True)
    @attr.s(auto_attribs=True)
    @derive(AllowNoneDict, exclude={"obj"})
    class Derived:
        string: str
    ```
    Now derived is equivalent to this:
    ```
    
    @attr_with_schema(register_as_scheme=True, strict=True)
    @attr.s(auto_attribs=True)
    class Derived:
        string: str
        integer: int = 0
    ```
    
    Test Plan:
    Test on the example in summary with both default and without values.
    Works when the derived class is empty. Works when the derived class has default values.
    
    Reviewers: arsen, hrach
    
    Reviewed By: arsen
    
    Differential Revision: https://phabricator.hilearn.io/D3710

--- README.md
+++ README.md
@@ -17,3 +17,29 @@ Register EnumField to api with
 ```
 api.register_field(EnumField, 'string', None)
 ```
+
+To derive from an `attrs` class use `derive` decorator. Here is an example:
+```
+@attr_with_schema(register_as_scheme=True, strict=True)
+@attr.s(auto_attribs=True)
+class Base:
+    obj: dict
+    integer: int = 0
+
+
+@attr_with_schema(register_as_scheme=True, strict=True)
+@attr.s(auto_attribs=True)
+@derive(AllowNoneDict, exclude={"obj"})
+class Derived:
+    string: str
+```
+
+Note, that `@derive` should be called first. `derive` get an additional `exclude` argument, which excludes already existing attributes from base class(it doesn't exclude from current class).
+The `Derived` class above is equivalent to the following:
+```
+@attr_with_schema(register_as_scheme=True, strict=True)
+@attr.s(auto_attribs=True)
+class Derived:
+    string: str
+    integer: int = 0
+```
--- marshenum/marsh_schema.py
+++ marshenum/marsh_schema.py
@@ -1,4 +1,6 @@
 import attr
+from copy import copy
+from functools import wraps
 import marshmallow as ma
 from marshmallow.exceptions import ValidationError
 from marshmallow_annotations.ext.attrs import AttrsSchema
@@ -8,7 +10,6 @@ def attr_with_schema(**kwargs):
     def decorator(cls):
         schema_meta = getattr(cls, 'SchemaMeta', object)
         fields = attr.fields(cls)
-
         class Schema(AttrsSchema):
             class Meta(schema_meta):
                 locals().update(kwargs)
@@ -26,4 +27,25 @@ def attr_with_schema(**kwargs):
 
         cls.__iter__ = attr_iter
         return cls
+    return decorator
+
+
+def derive(from_, *, exclude=None):
+    if exclude is None:
+        exclude = []
+    exclude = set(exclude)
+    def decorator(cls):
+        @wraps(cls, updated=[])
+        class Wrapped(cls):
+            locals().update({attr.name: attr.default
+                             for attr in from_.__attrs_attrs__
+                             if attr.name not in exclude})
+        Wrapped.__annotations__ = copy(getattr(cls, '__annotations__', {}))
+        for k in Wrapped.__annotations__.keys():
+            if hasattr(cls, k):
+                setattr(Wrapped, k, getattr(cls, k))
+        Wrapped.__annotations__.update({attr.name: attr.type
+                                        for attr in from_.__attrs_attrs__
+                                        if attr.name not in exclude})
+        return Wrapped
     return decorator
